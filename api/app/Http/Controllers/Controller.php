68bc9b0f3bafea4835f9a22d4ae9824bc9baa614
Anton <datskiiak@yandex.ru>
2025-06-30 16:01:51 +0400

    add swagger documentation

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(version: "1.0.0", title: "Task Manager API", description: "Description of Task Manager API")]
 abstract class Controller
 {
     //
