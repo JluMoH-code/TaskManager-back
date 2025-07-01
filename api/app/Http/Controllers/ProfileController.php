fac13f0557b56d4f739b817d0bb1df1f063f07b4
Anton <datskiiak@yandex.ru>
2025-07-01 10:10:43 +0400

    change status response

--- api/app/Http/Controllers/ProfileController.php
+++ api/app/Http/Controllers/ProfileController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ProfileResource;
 use Illuminate\Http\Request;
-use Log;
 use OpenApi\Attributes as OA;
 
 class ProfileController extends Controller
@@ -21,6 +20,6 @@ class ProfileController extends Controller
     public function show(Request $request) 
     {
         $user = $request->user();
-        return response()->json(new ProfileResource($user), 204);
+        return response()->json(new ProfileResource($user));
     }
 }
